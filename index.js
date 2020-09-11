c25399c76c740afd3a7ad20d186765e005883518
Paul Coe <pcoe@tesla.com>
2020-09-11 11:15:11 -0700

    Add initial db hookup
    Also add query inserting new users into db

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql2");
+
+const dbConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "supercool",
+  database: "vernaculearn"
+});
 
 
 const app = express();
@@ -8,6 +16,11 @@ const urlEncodedForms = express.urlencoded({extended: true});
 app.use(urlEncodedForms);
 
 app.get("/", serveRegistrationPage);
+
+app.get("/style.css", function(req,res){
+  res.sendFile( path.resolve("./docs/style.css") );
+});
+
 app.post("/receive-registration", processUserRegistration);
 
 app.listen(9999);
@@ -18,9 +31,23 @@ app.listen(9999);
 function processUserRegistration (request, response) {
     console.log("testing:", request.body);
 
+    dbConnection.query(
+
+      "INSERT INTO users (first_name, last_name, email, password, email_verified) VALUES (?,?,?,?,?);",
+
+      [request.body.firstName, request.body.lastName, request.body.emailAddress, request.body.password, 0],
+
+      function(err, results, fields) {
+        if (err) console.log(err);
+        console.log("results:", results)
+      }
+
+    );
+
+
     response.send("Yeah yeah, received your form!");
 }
 
 function serveRegistrationPage (request, response) {
-    response.sendFile( path.resolve("registration.html") );
+    response.sendFile( path.resolve("./docs/index.html") );
 }
