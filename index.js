79289b15510e9aa0fdef575d3de6881f7a11fff2
Paul Coe <pcoe@tesla.com>
2020-09-11 20:57:50 -0700

    Fix missing parenthesis bug

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const app = express();
 /* Tell application to use URL encoded forms */
 app.use( express.urlencoded( {extended: true} ) );
 
-app.get("/", require("./src/serveRegistrationPage");
+app.get("/", require("./src/serveRegistrationPage"));
 app.get("/style.css", require("./src/serveStyle"));
-app.post("/receive-registration", require("./src/processUserRegistration");
+app.post("/receive-registration", require("./src/processUserRegistration"));
 
 
 
