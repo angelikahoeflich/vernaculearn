5eb81e45654d5d5ca048c432e925c440ebb603c1
Paul Coe <pcoe@tesla.com>
2020-09-11 21:07:39 -0700

    Add /api/cards route and mess with comments

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-/* Requirements */
 const express = require("express");
-
-/* Create Express Application */
 const app = express();
 
-/* Tell application to use URL encoded forms */
 app.use( express.urlencoded( {extended: true} ) );
 
+
+/* "Web" Routes / "Pages" */
 app.get("/", require("./src/serveRegistrationPage"));
 app.get("/style.css", require("./src/serveStyle"));
 app.post("/receive-registration", require("./src/processUserRegistration"));
 
 
+/* "API" Routes */
+app.get("/api/cards", require("./src/getAllCards"));
 
 app.listen(9999);
+console.log("VernacuLearn in listening for HTTP on port 9999.");
 
