23223485c7499cf876d0c034c8e0ac8a8bb118b0
Paul Coe <pcoe@tesla.com>
2020-09-18 14:17:09 -0700

    Add module for getting a card by ID
    ...from the database.

---
+++ src/getCardById.js
@@ -0,0 +1,19 @@
+const database = require("../lib/database");
+
+module.exports = function(req, res) {
+
+  database.query(
+
+    "SELECT * FROM cards WHERE id=?;",
+
+    [req.params.id],
+
+    function(err, results, fields) {
+      if (err) { console.log("err", err) };
+
+      res.json(results)
+    }
+
+  );
+
+}
