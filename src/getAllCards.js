879b898ca66178836c8edb367adde6182066c619
Paul Coe <pcoe@tesla.com>
2020-09-18 14:18:33 -0700

    Only log err if there's an error

--- src/getAllCards.js
+++ src/getAllCards.js
@@ -2,12 +2,13 @@ const database = require("../lib/database");
 
 module.exports = function(req, res) {
 
+
   database.query(
 
     "SELECT * FROM cards;",
 
     function(err, results, fields) {
-      console.log("err", err);
+      if (err) { console.log("err", err) };
 
       res.json(results)
     }
