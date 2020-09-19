f4ba65ac7102a5b552b1a67d5188bfc23430df57
Paul Coe <paul@pauls-macbook.local>
2020-09-19 14:10:36 -0700

    Add interests routes to index.js

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.post("/receive-registration", require("./src/processUserRegistration"));
 /* "API" Routes */
 app.get("/api/cards/:id", require("./src/getCardById"));
 app.get("/api/cards", require("./src/getAllCards"));
+app.get("/api/interests/:id", require("./src/getInterestById"));
+app.get("/api/interests", require("./src/getAllInterests"));
 
 app.listen(9999);
 console.log("VernacuLearn is listening for HTTP on port 9999.");
